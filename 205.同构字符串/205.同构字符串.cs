e72abba931a03411d8d30efade7f5bb0c8cdc508
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2020-08-23 18:25:46 +0800

    Create 205.同构字符串.cs

---
+++ 205.同构字符串/205.同构字符串.cs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=205 lang=csharp
+ *
+ * [205] 同构字符串
+ */
+
+// @lc code=start
+public class Solution {
+    public bool IsIsomorphic(string s, string t) {
+        
+    }
+}
+// @lc code=end
+
