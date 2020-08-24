f980b7f59349c014ab053a62085644e9452a791c
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2020-08-24 15:48:49 +0800

    Update 205.同构字符串.cs

--- 205.同构字符串/205.同构字符串.cs
+++ 205.同构字符串/205.同构字符串.cs
@@ -5,10 +5,23 @@
  */
 
 // @lc code=start
-public class Solution {
-    public bool IsIsomorphic(string s, string t) {
-        
-    }
+using System.Collections.Generic;
+
+public class Solution
+{
+	public bool IsomorphicTo(string s, string t)
+	{
+		Dictionary<char, char> charMap = new Dictionary<char, char>();
+		for (int i = 0; i < s.Length; i++)
+			if (!charMap.ContainsKey(s[i]))
+				charMap.Add(s[i], t[i]);
+			else if (charMap[s[i]] != t[i])
+				return false;
+		return true;
+	}
+	public bool IsIsomorphic(string s, string t)
+	{
+		return IsomorphicTo(s, t) && IsomorphicTo(t, s);
+	}
 }
 // @lc code=end
-
