e27eb99efafeb93a30668bfb3e6ef4c0f32983b5
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2020-11-22 12:47:18 +0800

    Update 342.4-的幂-Bit.cs

--- 342.4-的幂/342.4-的幂-Bit.cs
+++ 342.4-的幂/342.4-的幂-Bit.cs
@@ -7,6 +7,9 @@
 // @lc code=start
 public class Solution
 {
-	public bool IsPowerOfFour(int num) => num > 0 && (num & (num - 1)) == 0 && (num & 0xaaaaaaaa) == 0;
+	public bool IsPowerOfFour(int num)
+		=> num > 0
+		&& (num & (num - 1)) == 0
+		&& (num & 0xaaaaaaaa) == 0;
 }
 // @lc code=end
