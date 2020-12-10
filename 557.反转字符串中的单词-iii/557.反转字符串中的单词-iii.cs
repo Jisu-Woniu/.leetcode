1e90481da814210066329e68d03d75d2695e640b
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2020-12-10 08:47:36 +0800

    Create 557.反转字符串中的单词-iii.cs

---
+++ 557.反转字符串中的单词-iii/557.反转字符串中的单词-iii.cs
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=557 lang=csharp
+ *
+ * [557] 反转字符串中的单词 III
+ */
+using System;
+// @lc code=start
+public class Solution {
+    public string ReverseWords(string s)
+	{
+		string[] words = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+		for (int i = 0; i < words.Length; i++)
+		{
+			char[] chars = words[i].ToCharArray();
+			Array.Reverse(chars);
+			words[i] = new string(chars);
+		}
+		return string.Join(' ', words);
+	}
+}
+// @lc code=end
+
