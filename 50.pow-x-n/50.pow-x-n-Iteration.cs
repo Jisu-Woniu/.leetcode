8a9395f723e54f243fc1e0a9448bf9171a9d9175
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2021-01-13 20:42:30 +0800

    Update 50.pow-x-n-Iteration.cs

--- 50.pow-x-n/50.pow-x-n-Iteration.cs
+++ 50.pow-x-n/50.pow-x-n-Iteration.cs
@@ -8,10 +8,10 @@
 public class Solution {
     public double MyPow(double x, int n)
 	{
-		if (n == 1) return x;
-		else if (n == -1) return 1 / x;
+		if(n == int.MinValue) return MyPow(1 / x / x, -(n >> 1));
+		else if (n < 0) return MyPow(1 / x, -n);
 		else if (n == 0) return 1;
-		double p;
+		double p = 1;
 		while (n != 0)
 		{
 			if ((n & 1) == 1)
@@ -19,6 +19,7 @@ public class Solution {
 			x *= x;
 			n >>= 1;
 		}
+		return p;
 	}
 }
 // @lc code=end
